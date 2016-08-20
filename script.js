1a48aff95ac48c68b37e3f022bcd950389951544
root <root>
2016-08-20 03:05:21 -0400

    add more headlines

--- script.js
+++ script.js
@@ -8,7 +8,47 @@ var list = [
 	/somebody needs to/i,
 	/you won['’]?t believe/i,
 	/what happens/i,
-	/^\d+/
+	/^\d+/,
+	/get rid of/i,
+	/secret of/i,
+	/shocking/i
+	/what you need to know/i,
+	/won['’]?t believe/i,
+	/pictures of/,
+	/things that will/i,
+	/nip slips/i,
+	/we need to talk about/i,
+	/signs you['’]?re/i,
+	/you can/i,
+	/what could possibly/i,
+	/when you read these/i,
+	/until you see/i,
+	/didn['’]t know what/i,
+	/what they found/i,
+	/what she found/i,
+	/what he found/i,
+	/who['’]d thougt/i,
+	/all she did was/i,
+	/all he did was/i,
+	/all they did was/i,
+	/what this/i,
+	/when he/i,
+	/when she/i,
+	/when they/i,
+	/when this man/i,
+	/when this woman/i,
+	/when this baby/i,
+	/when this child/i,
+	/when this puppy/i,
+	/when this dog/i,
+	/when this kitten/i,
+	/this man/i,
+	/this woman/i,
+	/this child/i,
+	/this baby/i,
+	/never realized/i,
+	/something great happened/i,
+	/something happened/i
 ];
 
 function test( string ) {
@@ -16,13 +56,12 @@ function test( string ) {
 	if ( string.length < 15 ) return false;
 
 	for ( var i = 0, l = list.length; i < l; i ++ ) {
-
+	
 		if ( list[ i ].test( string ) ) return true;
-
+	
 	}
 
 	return false;
-
 }
 
 function parseNode( element ) {
